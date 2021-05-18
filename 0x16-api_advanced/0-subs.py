19f3c9bd571692e6b10c27393ed51feb3386aca0
AteCastillo <atecast@gmail.com>
2021-05-18 15:35:08 -0300

    user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,8 +12,9 @@ def number_of_subscribers(subreddit):
         return(0)
     url = 'https://www.reddit.com'
     url_api = '{}/r/{}/about.json'.format(url, subreddit)
-    headers = {'user-agent': 'safari:holberton/0.1.0'}
-    response = requests.get(url_api, headers=headers, allow_redirects=False)
+    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:88.0)\
+                            Gecko/20100101 Firefox/88.0'}
+    response = requests.get(url_api, headers=headers)
     subscribers = response.json().get('data').get('subscribers')
     if response.status_code == 200:
         return subscribers
