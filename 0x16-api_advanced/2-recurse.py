a423db66cad52f339738121a4f0a82b5df733332
AteCastillo <atecast@gmail.com>
2021-05-18 22:47:12 -0300

    adding 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
-"""queries the Reddit API and returns a list containing
-the titles of all hot articles for a given subreddit recursively
 """
-
+Request the top ten hot posts
+"""
 import requests
 
-def recurse(subreddit, hot_list=[]):
-    """new"""
-    
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    Gets recursively the top hot posts in Reddit API
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    query = {'after': after, 'limit': 2}
+    response = requests.get(url,
+                            allow_redirects=False,
+                            params=query,
+                            headers={'User-Agent': 'MyMachintosh'})
+    if response and response.status_code < 300:
+        after = response.json().get('data').get('after')
+        if (after is None or len(hot_list) == 10):
+            return hot_list
+        post_list = response.json().get('data').get('children')
+        for children in post_list:
+            hot_list.append(children.get('data').get('title'))
+        return recurse(subreddit, hot_list, after)
+    else:
+        return None
